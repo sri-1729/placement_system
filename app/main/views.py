6ed90bad05c7c1daf0c8a42e3cdd0fc2cccc54ba
saimineesh786 <rmineesh@gmail.com>
2021-11-30 20:52:44 +0530

    indexing issue

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ def home():
 	if 'user' in session :
 		if session['user']['type_'] == 'company' :
 			sql = f"SELECT admin_id FROM COMPANY WHERE com_uid='{session['user']['userid']}'"
-			res = db.engine.execute(sql).first()
+			res = db.engine.execute(sql).first()[0]
 			if res == None:
 				msg='False'
 	return render_template('home.html',msg=msg)

1a34e0934d7fba3a95010cc802eb969825bbfb01
abc <abc.com>
2021-12-04 11:49:58 +0530

    Added comments

--- plms.py
+++ plms.py
@@ -5,6 +5,7 @@ from app.models import Login
 app = create_app('development')
 
 #run python script with shell_context_processor
+#redundant
 @app.shell_context_processor
 def make_shell_context():
 	return dict(db=db, Login=Login)
